f729c14067824252af5acb7fb2964317c41afbef
jfdsouza988 <juliosouza.rs@gmail.com>
2022-08-06 18:09:34 -0300

    feat: add implementation of UsersRepository

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,19 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    return await this.repository.findOneOrFail(user_id, {
+      relations: ['games']
+    });
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return await this.repository.query("SELECT * FROM users ORDER BY first_name");
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return await this.repository.query("SELECT * FROM users WHERE first_name ILIKE $1 and last_name ILIKE $2", [first_name, last_name]); 
   }
 }
